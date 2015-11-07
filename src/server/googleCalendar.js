e9f588c2a9a2a17564f3c34c14d3de84c777d09c
Christophe Hurpeau <christophe@hurpeau.com>
2015-11-07 01:09:28 +0100

    isBusy()

--- src/server/googleCalendar.js
+++ src/server/googleCalendar.js
@@ -12,8 +12,15 @@ const jwtClient = new googleAuth.JWT(key.client_email, null, key.private_key, sc
 const calendarApi = googleCalendar('v3');
 
 
-export function getEvents() {
-    (new Promise((resolve, reject) => {
+let _authenticated = false;
+
+function authenticate() {
+    if (_authenticated) {
+        return Promise.resolve();
+    }
+
+    _authenticated = true;
+    return new Promise((resolve, reject) => {
         jwtClient.authorize((err, tokens) => {
             if (err) {
                 return reject(err);
@@ -22,34 +29,47 @@ export function getEvents() {
             console.log(tokens);
             resolve();
         });
-    })).then(auth => {
+    });
+}
+
+function ask(method, params, retryCount = 2) {
+    return authenticate().then(() => {
+        params.auth = jwtClient;
         return new Promise((resolve, reject) => {
-            calendarApi.events.list({
-                auth: jwtClient,
-                calendarId: 'evaneos.com_64m6813jhu0v00r4j0o3lq388k@group.calendar.google.com',
-                timeMin: (new Date()).toISOString(),
-                maxResults: 10,
-                singleEvents: true,
-                orderBy: 'startTime',
-            }, (err, response) => {
+            method(params, (err, response) => {
                 if (err) {
                     return reject(err);
                 }
 
                 return resolve(response);
-            })
+            });
         });
     });
-    /*.then(({ items }) => {
-        if (items.length == 0) {
-            console.log('No upcoming events found.');
-        } else {
-            console.log('Upcoming 10 events:');
-            for (var i = 0; i < items.length; i++) {
-                var event = items[i];
-                var start = event.start.dateTime || event.start.date;
-                console.log('%s - %s', start, event.summary);
-            }
-        }
-    })*///.catch((err) => console.log(err.stack));
+}
+
+export async function isBusy() {
+    const events = await getEvents();
+    return events && events.length && events[0].startDate.getTime() < Date.now() || false;
+}
+
+export function getEvents() {
+    return ask(calendarApi.events.list, {
+        calendarId: 'evaneos.com_cl7v6mtsfuh4lq7r4vrgt0faqs@group.calendar.google.com',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 2,
+        singleEvents: true,
+        orderBy: 'startTime',
+    })
+    .then(({ items }) => {
+        return items.map(item => ({
+            id: item.id,
+            status: item.status,
+            startDate: new Date(item.start.dateTime),
+            endDate: new Date(item.end.dateTime),
+        }));
+    });
+    //.then((items) => console.log(items))
+    //.catch((err) => console.log(err.stack));
 };
+
+
